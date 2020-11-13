b0b4476954fda754f5c150ec556889f33b8a5062
Administrator <1197829331@qq.com>
2020-11-13 13:23:02 +0800

    init new

--- src/App.js
+++ src/App.js
@@ -10,37 +10,8 @@ class App extends React.Component {
         return (
             <div className="viewer">
                 <div className="container">
-                    <div className="tile is-ancestor">
-                        <div className="tile is-vertical is-8">
-                            <div className="tile">
-                                <div className="tile is-parent is-vertical">
-                                    <article className="tile is-child box">
-
-                                    </article>
-                                    <article className="tile is-child box">
-
-                                    </article>
-                                </div>
-                                <div className="tile is-parent">
-                                    <article className="tile is-child box">
-
-                                    </article>
-                                </div>
-                            </div>
-                            <div className="tile is-parent">
-                                <article className="tile is-child box">
-
-                                </article>
-                            </div>
-                        </div>
-                        <div className="tile is-parent">
-                            <article className="tile is-child box">
-
-                            </article>
-                        </div>
-                    </div>
+                    o
                 </div>
-
                 <footer className="footer">
                     <div className="content has-text-centered">
                         <IcpLabel/>
