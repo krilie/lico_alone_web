9b9a27898c860e127145fda5bce2d83d7026f9ac
Administrator <1197829331@qq.com>
2020-11-13 18:06:58 +0800

    init new

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import ArticlePage from "../article/ArticlePage";
+import AppVersion from "../../components/AppVersion";
 
 class HomePage extends Component {
     render() {
         return (
             <div>
                 <ArticlePage/>
+                <AppVersion/>
             </div>
         );
     }
