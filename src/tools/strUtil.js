cfcee112ea2ab5fdfc69faa96029eb24f01e9691
Administrator <1197829331@qq.com>
2020-11-20 16:33:40 +0800

    pic picture

--- src/tools/strUtil.js
+++ src/tools/strUtil.js
@@ -2,9 +2,9 @@ export function stringToHex(str) {
     if (str === undefined || str === {} || str === "")
         str = " "
     const buf = Buffer.from(str, 'utf8');
-    return buf.toString('hex')
+    return buf.toString('base64')
 }
 
 export function hexToString(str) {
-    return Buffer.from(str, 'hex').toString('utf8');
+    return Buffer.from(str, 'base64').toString('utf8');
 }
