3f10c7ccac6b9805199599f8ccd933778899a9b6
Administrator <1197829331@qq.com>
2020-11-20 15:28:24 +0800

    'ooooo'

--- src/tools/strUtil.js
+++ src/tools/strUtil.js
@@ -1,20 +1,10 @@
 export function stringToHex(str) {
     if (str === undefined || str === {} || str === "")
         str = " "
-    var val = "";
-    for (var i = 0; i < str.length; i++) {
-        if (val == "")
-            val = str.charCodeAt(i).toString(16);
-        else
-            val += "," + str.charCodeAt(i).toString(16);
-    }
-    return val;
+    const buf = Buffer.from(str, 'utf8');
+    return buf.toString('hex')
 }
 
 export function hexToString(str) {
-    var val = "";
-    var arr = str.split(",");
-    for (var i = 0; i < arr.length; i++)
-        val += arr[i].fromCharCode(i);
-    return val;
+    return Buffer.from(str, 'hex').toString('utf8');
 }
