6f9f99165239576195edf2179486ce5a5b4c9334
Administrator <1197829331@qq.com>
2020-11-25 09:09:13 +0800

    go back

--- src/components/GoBackToolBar.js
+++ src/components/GoBackToolBar.js
@@ -4,10 +4,10 @@ import "./GoBackToolBar.css"
 export default function GoBackToolBar(props) {
     return (
         <div className="go-back-toolbar">
-            <div title="返回"
+            <div title="返回上一级"
                  onClick={e => props.history.goBack()}
                  className="go-back-button button is-light">
-                <strong>⇐&nbsp;&nbsp;返回上一级</strong></div>
+                <strong>⇐&nbsp;&nbsp;返回</strong></div>
         </div>
     );
 }
