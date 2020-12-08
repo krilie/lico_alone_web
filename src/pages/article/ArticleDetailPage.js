58796963934fb71bf4eb5dcf7453d01e835c567e
Administrator <1197829331@qq.com>
2020-12-08 14:42:12 +0800

    like dislike

--- src/pages/article/ArticleDetailPage.js
+++ src/pages/article/ArticleDetailPage.js
@@ -3,6 +3,8 @@ import {checkResDataWithToast, getArticleById} from "../../api/apiCommon";
 import GoBackToolBar from "../../components/GoBackToolBar";
 import "./ArticleDetailPage.css"
 import "./ArticleDetailPageContentView.css"
+import {FiThumbsUp, FiThumbsDown} from "react-icons/fi";
+
 class ArticleDetailPage extends Component {
 
     constructor(props) {
@@ -41,9 +43,35 @@ class ArticleDetailPage extends Component {
 
     render() {
         const {article, loading} = this.state
+        const thumbsUpView = article.has_like ?
+            <div className="thumbs-has-view">
+                <FiThumbsUp size={30}/>
+                <div style={{textAlign: "center"}}>{article.like}</div>
+            </div>
+            :
+            <div className="thumbs-no-has-view">
+                <FiThumbsUp size={30}/>
+                <div style={{textAlign: "center"}}>{article.like}</div>
+            </div>
+        const thumbsDownView = article.has_dislike ?
+            <div className="thumbs-has-view">
+                <FiThumbsDown size={30}/>
+                <div style={{textAlign: "center"}}>{article.like}</div>
+            </div>
+            :
+            <div className="thumbs-no-has-view">
+                <FiThumbsDown size={30}/>
+                <div style={{textAlign: "center"}}>{article.like}</div>
+            </div>
+
         const articleView = loading === false ?
             <div className="article-view">
-                <div id="articleView" className="article-view-content ck-content" dangerouslySetInnerHTML={{__html: article.content}}/>
+                <div id="articleView" className="article-view-content ck-content"
+                     dangerouslySetInnerHTML={{__html: article.content}}/>
+                <div className="article-view-thumbs-view">
+                    {thumbsUpView}
+                    {thumbsDownView}
+                </div>
             </div>
             :
             <div className="article-view-loading">loading...</div>
