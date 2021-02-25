d9804ebef9071cc6db345def53905a4a24b5a137
livo <1197829331@qq.com>
2021-02-25 11:55:18 +0800

    foot 修改

--- src/components/IcpLabel.js
+++ src/components/IcpLabel.js
@@ -1,27 +1,10 @@
 import React from "react";
 import "./Icplable.css"
-import {getIcpInfo} from "../api/apiCommon";
-import {checkResDataWithToast} from "../api/apiBase";
 
 export default class IcpLabel extends React.Component {
 
-    state = {
-        name: "", link: "", label: "",
-    }
-
-    componentDidMount() {
-        getIcpInfo().then(res => {
-            let data = checkResDataWithToast(res)
-            if (data) {
-                this.setState({
-                    ...data
-                })
-            }
-        })
-    }
-
     render() {
-        const {name, link, label} = this.state;
+        const {name, link, label} = this.props;
         return (
             <a className="icpLabelDiv"
                title={label}
--- src/components/WebFooter.js
+++ src/components/WebFooter.js
@@ -1,17 +1,42 @@
 import React, {Component} from 'react';
-import IcpLabel from "./IcpLabel";
+import {getIcpInfo} from "../api/apiCommon";
+import {checkResDataWithToast} from "../api/apiBase";
 
 class WebFooter extends Component {
+
+    state = {
+        name: "", link: "", label: "",domain:""
+    }
+
+    componentDidMount() {
+        getIcpInfo().then(res => {
+            let data = checkResDataWithToast(res)
+            if (data) {
+                this.setState({
+                    ...data
+                })
+            }
+        })
+    }
+
+
     render() {
         return (
             <div style={{
                 display: "flex",
+                flexDirection: "row",
                 justifyContent: "center",
                 alignItems: "center",
                 padding: "1rem 1rem 1rem",
                 backgroundColor: "transparent"
             }}>
-                <IcpLabel/>
+                <div style={{color:"#1acaef"}}>@ 2019-2021 {this.state.domain} 版权所有</div>
+                <pre>  </pre>
+                <a className="icpLabelDiv"
+                   title={this.state.label}
+                   href={this.state.link}
+                   target="_blank"
+                   rel="noopener noreferrer">{this.state.name}</a>
             </div>
         );
     }
